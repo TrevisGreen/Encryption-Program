1a1e53931d15e6351af4777d4e36c7fb61bb795b
TrevisGreen <Trevis.Green@gmail.com>
2023-02-14 02:52:54 -0600

    Declared global variables, worked on constructor, and added functionality to askQuestion().

--- src/EncryptionProgram.java
+++ src/EncryptionProgram.java
@@ -1,11 +1,57 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class EncryptionProgram {
 
-    EncryptionProgram() {
+    private Scanner scanner;
+    private Random random;
+    private ArrayList<Character> list;
+    private ArrayList<Character> shuffledList;
+    private char character;
+    private String line;
+    private char[] letters;
+    private char[] secretLetters;
 
+    EncryptionProgram() {
+        scanner = new Scanner(System.in);
+        random = new Random();
+        list = new ArrayList();
+        shuffledList = new ArrayList();
+        character = ' ';
+
+        newKey();
+        askQuestion();
     }
 
     private void askQuestion() {
+        while (true) {
+            System.out.println("************************************************");
+            System.out.println("What do you want to do?");
+            System.out.println("(N)ewKey, (G)etKey, (E)ncrypt, (D)ecrypt, (Q)uit");
+            char response = Character.toUpperCase(scanner.nextLine().charAt(0));
 
+            switch (response) {
+                case 'N':
+                    newKey();
+                    break;
+                case 'G':
+                    getKey();
+                    break;
+                case 'E':
+                    encrypt();
+                    break;
+                case 'D':
+                    decrypt();
+                    break;
+                case 'Q':
+                    quit();
+                    break;
+                default:
+                    System.out.println("Not a valid answer :(");
+
+            }
+        }
     }
 
     private void newKey() {
@@ -25,6 +71,6 @@ public class EncryptionProgram {
     }
 
     private void quit() {
-        
+
     }
 }
