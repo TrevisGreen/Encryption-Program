8044b56814fd89667cf3df49aa210f0323600a47
TrevisGreen <Trevis.Green@gmail.com>
2023-02-14 06:40:46 -0600

    Done with program

--- src/EncryptionProgram.java
+++ src/EncryptionProgram.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,7 +12,6 @@ public class EncryptionProgram {
     private char character;
     private String line;
     private char[] letters;
-    private char[] secretLetters;
 
     EncryptionProgram() {
         scanner = new Scanner(System.in);
@@ -56,21 +56,76 @@ public class EncryptionProgram {
 
     private void newKey() {
 
+        character = ' ';
+        list.clear();
+        shuffledList.clear();
+
+        for(int i = 32; i < 127; i++) {
+            list.add(Character.valueOf(character));
+            character++;
+        }
+
+        shuffledList = new ArrayList(list);
+        Collections.shuffle(shuffledList);
+        System.out.println("A new key has been generated");
     }
 
     private void getKey() {
-
+        System.out.println("Key: ");
+        for(Character x: list) {
+            System.out.print(x);
+        }
+        System.out.println();
+        for(Character x: shuffledList) {
+            System.out.print(x);
+        }
+        System.out.println();
     }
 
     private void encrypt() {
+        System.out.println("Enter a message to be encrypted: ");
+        String message = scanner.nextLine();
 
+        letters = message.toCharArray();
+
+        for(int i = 0; i < letters.length; i++) {
+            for(int j = 0; j < list.size(); j++) {
+                if(letters[i] == list.get(j)) {
+                    letters[i] = shuffledList.get(j);
+                    break;
+                }
+            }
+        }
+        System.out.println("Encrypted: ");
+        for(char x : letters) {
+            System.out.print(x);
+        }
+        System.out.println();
     }
 
     private void decrypt() {
+        System.out.println("Enter a message to be decrypted: ");
+        String message = scanner.nextLine();
 
+        letters = message.toCharArray();
+
+        for(int i = 0; i < letters.length; i++) {
+            for(int j = 0; j < shuffledList.size(); j++) {
+                if(letters[i] == shuffledList.get(j)) {
+                    letters[i] = list.get(j);
+                    break;
+                }
+            }
+        }
+        System.out.println("Decrypted: ");
+        for(char x : letters) {
+            System.out.print(x);
+        }
+        System.out.println();
     }
 
     private void quit() {
-
+        System.out.println(" Thank you, have a nice day");
+        System.exit(0);
     }
 }
